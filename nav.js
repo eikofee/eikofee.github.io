5fe464e03ad4db4db21f2352251c825f439043c5
Echoffee <light.runa@gmail.com>
2017-05-14 16:48:09 +0200

    Update

--- nav.js
+++ nav.js
@@ -5,6 +5,7 @@ document.write('\
 <nav id="navbar">\
 	<ul>\
 		<li><a href="index.html">Home</a></li>\
+		<li><a href="about.html">About</a></li>\
 		<li><a href="projects.html">Projects</a></li>\
 		<li><a href="contact.html">Contact</a></li>\
 	</ul>\

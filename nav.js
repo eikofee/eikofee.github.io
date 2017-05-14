ee216dc100a9b0d3d6cb74dd66d6d7b631eec370
Echoffee <light.runa@gmail.com>
2017-05-14 15:39:50 +0200

    Update

---
+++ nav.js
@@ -0,0 +1,12 @@
+document.write('\
+<div id="cover">\
+	Adrien Halnaut\
+</div>\
+<nav id="navbar">\
+	<ul>\
+		<li><a href="index.html">Home</a></li>\
+		<li><a href="projects.html">Projects</a></li>\
+		<li><a href="contact.html">Contact</a></li>\
+	</ul>\
+</nav>\
+');
